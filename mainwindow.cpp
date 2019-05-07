73e43a9395a18d3315656f67ad066f6b55757c40
Mikhail Reshetnyak <micresh@gmail.com>
2019-05-07 23:28:42 +0300

    add timer and graphics item

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
 
+    ui->setupUi(this);
+    QBrush whiteBrush(Qt::white);
+    QPen blackPen(Qt::black);
+    scene = new QGraphicsScene(this);
+    timer = new QTimer(this);
+    ui->graphicsView->setScene(scene);
+    connect(timer, SIGNAL(timeout()), this, SLOT(timer_update()));
+    timer->setInterval(500);
+
+    //Rect = scene->addRect(10, 10, 50, 70, blackPen, whiteBrush);
+    Line = scene->addLine(10,10,10,80,blackPen);
 
 }
 
@@ -15,9 +26,20 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::paintEvent(QPaintEvent *e)
-{
-    QPainter painter(this);
-    painter.drawLine(QPoint(100,100), QPoint(100,300));
 
+void MainWindow::timer_update()
+{
+    Line->setRotation(x);
+    x += 5;
+    //Line->setPos(Line->x()*sin(Line->x()+1), Line->y()*cos(Line->x()+1));
+}
+
+void MainWindow::on_stop_btn_clicked()
+{
+    timer->stop();
+}
+
+void MainWindow::on_start_btn_clicked()
+{
+   timer->start();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,12 +2,17 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsItem>
 #include <QPainter>
 #include <QtGui>
 #include <QtCore>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
+
 }
 
 class MainWindow : public QMainWindow
@@ -20,8 +25,18 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    QGraphicsScene *scene;
+    QGraphicsRectItem *Rect;
+    QGraphicsLineItem *Line;
+    QTimer *timer;
+        int x = 5;
 protected:
-    void paintEvent(QPaintEvent *e);
+   // void paintEvent(QPaintEvent *e);
+private slots:
+    void timer_update();
+    void on_stop_btn_clicked();
+    void on_start_btn_clicked();
 };
 
 #endif // MAINWINDOW_H
